5fe7f09bc4118a66d3e3c0b98c6bbac1a6735f8f
EJ <sbjdnm@gmail.com>
2024-10-29 23:33:56 +0900

    Feat : 최초의 랜덤데이터 로컬저장

--- src/app/components/RecipesList.tsx
+++ src/app/components/RecipesList.tsx
@@ -7,14 +7,21 @@ import Image from "next/image";
 const RecipesList = () => {
   const { recipesList } = useRecipesList();
   console.log(recipesList);
+
+  if (!Array.isArray(recipesList)) {
+    return <p>오류발생</p>;
+  }
   return (
     <div>
-      <Image
-        src={recipesList[0]?.image}
-        alt=""
-        width={556} // 실제 너비
-        height={370}
-      />
+      {recipesList?.map((recipe: any) => (
+        <Image
+          key={recipe?.id}
+          src={recipe?.image}
+          alt=""
+          width={556} // 실제 너비
+          height={370}
+        />
+      ))}
     </div>
   );
 };
--- src/app/hooks/useRecipesList.ts
+++ src/app/hooks/useRecipesList.ts
@@ -6,7 +6,17 @@ import { useMemo } from "react";
 export const useRecipesList = () => {
   const { data: res, isLoading, isError } = useRecipesListQuery();
 
-  const recipesList = useMemo(() => res?.recipes ?? [], [res]);
+  const recipesList = useMemo(() => {
+    const savedRecipes = localStorage.getItem("recipesList");
 
+    if (savedRecipes) {
+      return JSON.parse(savedRecipes);
+    }
+
+    localStorage.setItem("recipesList", JSON.stringify(res?.recipes));
+    return res?.recipes;
+  }, [res]);
+
+  //로컬에 저장 , 로컬에 이미 있으면 더이상 안불러오는거지
   return { recipesList, isLoading, isError };
 };
